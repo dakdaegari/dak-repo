65185dc7eaf4cc2b65dd7447d466f0176fb51164
dakdaegari <dakdaegari@hotmail.com>
2012-06-05 18:39:17 +0900

    second test

--- github_test/src/Hello.java
+++ github_test/src/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("Hello Git");
 	}
 
 }
